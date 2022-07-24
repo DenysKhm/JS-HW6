dd92778fd05fe0060beb768623ce9d10d6449ee3
DenysKhm <106743435+DenysKhm@users.noreply.github.com>
2022-07-24 17:22:19 +0300

    task1

---
+++ js/task-01.js
@@ -0,0 +1,23 @@
+const title = document.querySelectorAll(".item");
+console.log(`Number of categories: ${title.length}`);
+
+const item = document.querySelector(".item");
+const firstItem = item.firstElementChild;
+console.log(`Category: ${firstItem.textContent}`);
+
+const itemAllLi = item.lastElementChild.children;
+console.log(`Elements: ${itemAllLi.length}`);
+
+const secondItem = item.nextElementSibling;
+const secondItemUl = secondItem.firstElementChild;
+console.log(`Category: ${secondItemUl.textContent}`);
+
+const secondItemLi = secondItem.lastElementChild.children;
+console.log(`Elements: ${secondItemLi.length}`);
+
+const thirdItem = secondItem.nextElementSibling;
+const thirdItemUl = thirdItem.firstElementChild;
+console.log(`Category: ${thirdItemUl.textContent}`);
+
+const thirdItemLi = thirdItem.lastElementChild.children;
+console.log(`Elements: ${thirdItemLi.length}`);
