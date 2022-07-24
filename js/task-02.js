39dea5873dfde60156317079166e9c0c08c037c2
DenysKhm <106743435+DenysKhm@users.noreply.github.com>
2022-07-24 18:35:21 +0300

    task2

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,13 @@
-const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
-];
+const ingredients = ["Potatoes", "Mushrooms", "Garlic", "Tomatos", "Herbs", "Condiments"];
+
+const ingredientsItem = document.querySelector("#ingredients");
+const elements = [];
+
+for (let item of ingredients) {
+	const createLi = document.createElement("li");
+	createLi.textContent = item;
+	createLi.classList.add("item");
+	elements.push(createLi);
+}
+
+ingredientsItem.append(...elements);
