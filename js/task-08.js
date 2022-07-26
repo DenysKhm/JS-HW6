b4fc6900cd7ffc64a1229f5064239823df17a3a1
DenysKhm <106743435+DenysKhm@users.noreply.github.com>
2022-07-26 17:35:37 +0300

    task8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,14 @@
+const loginForm = document.querySelector(".login-form");
 
+loginForm.addEventListener("submit", event => {
+	event.preventDefault();
+	if (loginForm.elements.email.value === "" || loginForm.elements.password.value === "") {
+		alert("Усі поля повинні бути заповнені");
+	} else {
+		{
+			console.log(`Email: ${loginForm.elements.email.value}, 
+            Password: ${loginForm.elements.password.value}`);
+			loginForm.reset();
+		}
+	}
+});
