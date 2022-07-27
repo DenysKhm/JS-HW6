7251d1b7e98719523e4cef7dab5135abd5bd3ad1
DenysKhm <106743435+DenysKhm@users.noreply.github.com>
2022-07-27 15:22:33 +0300

    task5

--- js/task-05.js
+++ js/task-05.js
@@ -2,7 +2,7 @@ const nameInput = document.querySelector("#name-input");
 const nameOutput = document.querySelector("#name-output");
 
 nameInput.addEventListener("input", event => {
-	if (!event.target.value) {
+	if (!event.target.value.trim()) {
 		nameOutput.textContent = "Anonymous";
 	} else {
 		nameOutput.textContent = event.currentTarget.value;
